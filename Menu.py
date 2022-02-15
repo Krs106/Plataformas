41d289bacc61bd2c9b2177d7482b79b1f04d335d
Krs106 <98362978+Krs106@users.noreply.github.com>
2022-02-14 18:46:18 -0600

    Update Menu.py
    Esta es una buena base de menu

--- Menu.py
+++ Menu.py
@@ -1,23 +1,98 @@
-mport pygame
+#!/usr/bin/python3.4
+# Setup Python ----------------------------------------------- #
+import pygame, sys
 
-def generar_otra_pantalla():
-    otra_pantalla = True
-    while otra_pantalla:
+# Setup pygame/window ---------------------------------------- #
+mainClock = pygame.time.Clock()
+from pygame.locals import *
+
+pygame.init()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500), 0, 32)
+
+font = pygame.font.SysFont(None, 20)
+
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+
+click = False
+
+
+def main_menu():
+    while True:
+
+        screen.fill((0, 0, 0))
+        draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
+
+        mx, my = pygame.mouse.get_pos()
+
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                import Sudoku_solve
+        if button_2.collidepoint((mx, my)):
+            if click:
+                options()
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+
+        click = False
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                #IMPORTANTE ESTA ACCION, DE MODO QUE SI PULSAN DE NUEVO LA TECLA SE OCULTE EL MENU
-                    otra_pantalla = False
-                # COLOCAMOS EL COLOR DEL MENU
-        screen.fill(WHITE)
-        # DIBUJAMOS COSAS
-        dibujar("cuadrado")
-        dibujar("menu de acciones")
-        # ACTUALIZAMOS EL DISPLAY CON UPDATE SE USA AQUI PARA 
-        # UNICAMENTE ACTUALIZAR LA PORCION DE PANTALLA QUE ESTAMOS USANDO, NO TODO.
-        # PORQUE DE LO CONTRARIO BORRARIA LO QUE ESTABA DEBAJO, 
-        # DIGAMOS ERA UN MENU DE PAUSA, SI USAMOS FLIP CUANDO QUITEMOS EL MENU NO QUEDARA NADA
-        pygame.display.update()
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
 
-        # Y LUEGO PONEMOS EL RELOJ RAPIDO PARA QUE SEA UN RENDERIZADO PERMANENTE
-        reloj_juego.tick(5)
+        pygame.display.update()
+        mainClock.tick(60)
+
+
+def game():
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+
+        draw_text('game', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+
+def options():
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+
+        draw_text('options', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+
+main_menu()
