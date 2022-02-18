ccf87ad0388513d483da72c41e1f418c59d45ea5
Krs106 <98362978+Krs106@users.noreply.github.com>
2022-02-17 21:23:01 -0600

    Update facil3.py

--- facil3.py
+++ facil3.py
@@ -161,7 +161,6 @@ def redraw_window(win, board, time, strikes):
 def format_time(secs):
     sec = secs % 60
     minute = secs//60
-    hour = minute//60
 
     mat = " " + str(minute) + ":" + str(sec)
     return mat
