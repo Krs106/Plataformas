b3ddefc1bef351346d490c992d8185a324c22ba8
Krs106 <98362978+Krs106@users.noreply.github.com>
2022-02-19 20:40:50 -0600

    Update Instru.py

--- Instru.py
+++ Instru.py
@@ -12,6 +12,15 @@ screen =pygame.display.set_mode([500,500])
 fondo = pygame.image.load("sudo.JPG").convert()
 text2 = font.render(("Digite su nombre"), True, black)
 base_font = pygame.font.Font(None,27)
+mx, my = pygame.mouse.get_pos()
+    menu = pygame.Rect(150, 450, 250, 50)
+    if menu.collidepoint(mx, my):
+        draw.rect(screen, (112, 185, 230), menu, 0)
+    else:
+        draw.rect(screen, (111, 161, 252), menu, 0)
+    text = font.render("Menú", True, (255, 255, 255))
+    screen.blit(text, (menu.x + (menu.width - text.get_width()) / 2,
+                    menu.y + (menu.height - text.get_height()) / 2))
 
 user_text1 = 'Instruciones'
 user_text2 = '1. Seleccione la celda con el mouse para modificar'
@@ -28,15 +37,6 @@ input_rect = pygame.Rect(200,135,140,32)
 color = pygame.Color('blue')
 
 while True:
-    mx, my = pygame.mouse.get_pos()
-    menu = pygame.Rect(150, 450, 250, 50)
-    if menu.collidepoint(mx, my):
-        draw.rect(screen, (112, 185, 230), menu, 0)
-    else:
-        draw.rect(screen, (111, 161, 252), menu, 0)
-    text = font.render("Menú", True, (255, 255, 255))
-    screen.blit(text, (menu.x + (menu.width - text.get_width()) / 2,
-                    menu.y + (menu.height - text.get_height()) / 2))
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             if menu.collidepoint(mouse.get_pos()):
