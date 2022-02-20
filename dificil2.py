885f6c4441f25586997698fe91468d5a226f87e6
Krs106 <98362978+Krs106@users.noreply.github.com>
2022-02-19 18:03:48 -0600

    Update dificil2.py

--- dificil2.py
+++ dificil2.py
@@ -223,6 +223,7 @@ def main():
 
                         if board.is_finished() or strikes == 5:
                             print("Game over")
+                            import Menu
                             importlib.reload(Menu)
 
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -233,6 +234,7 @@ def main():
                     key = None
                 if menu.collidepoint(mouse.get_pos()):
                     pygame.mixer.stop()
+                    import Menu
                     importlib.reload(Menu)
 
         if board.selected and key != None:
