d48f32e5a9ff906217ab1a54917a9ce549c45b80
Krs106 <98362978+Krs106@users.noreply.github.com>
2022-02-22 14:27:12 -0600

    Update Registro1.py

--- Registro1.py
+++ Registro1.py
@@ -189,7 +189,7 @@ def main():
         screen.fill((30, 30, 30))
         screen.blit(fondo, [0, 0])
         # Render the current text.
-        txt_surface = font.render(text, True, color)
+        txt_surface = font.render(text, True, 128, 0, 128)
         # Resize the box if the text is too long.
         width = max(210, txt_surface.get_width()+10)
         input_box.w = width
