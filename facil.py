33281a3b8c6a878b25793a708273bbd23562ac06
Krs106 <98362978+Krs106@users.noreply.github.com>
2022-02-22 12:24:50 -0600

    Update facil.py

--- facil.py
+++ facil.py
@@ -352,8 +352,7 @@ def main():
             if event.type == MOUSEBUTTONDOWN and event.button == 1:
                 if menu.collidepoint(mouse.get_pos()):
                     pygame.mixer.stop()
-                    import Menu
-                    importlib.reload(Menu)
+                    Menu()
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.KEYDOWN:
@@ -390,8 +389,7 @@ def main():
 
                         if board.is_finished() or strikes == 5:
                             print("Game over")
-                            import Menu
-                            importlib.reload(Menu)
+                            Menu()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
