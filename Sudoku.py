30fb22582f845e0abf58498dddb3baaae128e9ca
Krs106 <98362978+Krs106@users.noreply.github.com>
2022-02-23 11:17:31 -0600

    Update Sudoku.py
    Comentarios

--- Sudoku.py
+++ Sudoku.py
@@ -1,11 +1,12 @@
 
 def solve(tab):
+    # Revisa si hay un espacio vacio
     find = encontrar_vacio(tab)
     if not find:
         return True
     else:
         fila, col = find
-
+    # Rellena el sudoku con los valores validos
     for i in range(1,10):
         if valid(tab, i, (fila, col)):
             tab[fila][col] = i
@@ -17,6 +18,7 @@ def solve(tab):
 
     return False
 
+# Función para validar el valor que debe ir en cada espacio vacio
 def valid(tab, num, pos):
     #  Revisar fila
     for i in range(len(tab[0])):
@@ -39,6 +41,7 @@ def valid(tab, num, pos):
 
     return True
 
+# Imprime la tabla
 def imprimir_tabla(tab):
     for i in range(len(tab)):
         if i % 3 == 0 and i != 0:
@@ -53,6 +56,7 @@ def imprimir_tabla(tab):
             else:
                 print(str(tab[i][j]) + " ", end="")
 
+# Funcion encuentra un espacio vacio, los cuales son los 0 en la plantilla.
 def encontrar_vacio(tab):
     for i in range(len(tab)):
         for j in range(len(tab[0])):
